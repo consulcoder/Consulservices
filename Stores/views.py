30dc049dcfd45a909735c8559a3fa533f26846d8
RoheMMr0 <hromario286@gmail.com>
2024-11-01 17:55:41 -0400

    fix: cambio_de_pueba

--- Stores/views.py
+++ Stores/views.py
@@ -7,3 +7,4 @@ from .serializers import StoreSerializer
 class StoreViewSet(viewsets.ModelViewSet):
     queryset = Store.objects.all()
     serializer_class = StoreSerializer
+    ##fkjajflajsflasj
